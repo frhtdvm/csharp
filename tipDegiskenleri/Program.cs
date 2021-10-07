9dc84ff2bf2f7a608fa495feaf7220e7ba188c9c
blaze187 <frhtdvm@gmail.com>
2021-10-07 17:29:32 +0200

    operatör ve degiskenler eklendi

---
+++ tipDegiskenleri/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tipDegiskenleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expli Conversion (bilincli dönüsüm)
+            int x =4;
+            byte y =(byte) x;
+            Console.WriteLine("y:" + y);
+
+        }
+    }
+}
