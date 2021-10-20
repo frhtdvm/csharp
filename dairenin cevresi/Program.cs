c3de264cbc387832ce902c15f022e6eca064672c
blaze187 <frhtdvm@gmail.com>
2021-10-20 19:48:56 +0200

    degisik örnekler

---
+++ dairenin cevresi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dairenin_cevresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dairenin cevresi ile alanini hesaplayiniz
+            //pi sayisi, cevresi ve alani
+            const double PI = 3.14;
+            double daireninYariCapi;
+
+            Console.WriteLine("Dairenin yari capini giriniz");
+            daireninYariCapi = Convert.ToDouble(Console.ReadLine());
+
+            double DaireninCevresi = 2 * PI * daireninYariCapi;
+            double DaireninAlani = PI * daireninYariCapi * daireninYariCapi;
+
+            Console.WriteLine("Dairenin Cevresi = " + DaireninCevresi);
+            Console.WriteLine("Dairenin Alani = " + DaireninAlani);
+            
+
+        }
+    }
+}
---
+++ matematiksel Denklem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace matematiksel_Denklem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //matematiksel denklem cözme
+
+            /*
+            fonksiyon denklemi = f(x) = ax^2 + b + c -> 4x^2+2x+3
+            delta = b^2 - 4ac -> 4-4*4*3 = -44 (kökleri bulmak icin kullanilir)
+            f(2) f(4)
+            */
+            double a,b,c,x,sonuc,delta;
+
+            Console.WriteLine("Lütfen fonksiyonun a b c d ve x degerlerini sirasiyla giriniz");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
+
+            sonuc = a*x*+b*x+c;
+            delta = b*b-4*a*c;
+
+            Console.WriteLine("f({0})= " + sonuc + "Delta = " + delta,x);
+            
+        }
+    }
+}
