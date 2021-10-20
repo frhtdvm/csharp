ad06d3cc2b65ff008e126f7f2e0f5a18c98f9450
blaze187 <frhtdvm@gmail.com>
2021-10-20 20:17:24 +0200

    Matematik hazir fonksiyonlari eklendi

---
+++ mat hazir metotlar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace mat_hazir_metotlar
+{
+    class Program
+    {
+        //math sinifi
+        static void Main(string[] args)
+        {
+            double sayi = 16;
+
+            Console.WriteLine(Math.Pow(sayi, 3));
+            Console.WriteLine(Math.Max(sayi,-18));
+
+            double sayi2 = 4.5;
+            sayi2 = Math.Ceiling(sayi2);
+            Console.WriteLine(sayi2);
+            //Console.Writeline(Math.Floor(sayi2)+ " " + Math.Ceiling(sayi2));
+        }
+    }
+}
