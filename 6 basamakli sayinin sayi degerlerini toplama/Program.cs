6db29aef320414346d79573e15dfc4065949e558
blaze187 <frhtdvm@gmail.com>
2021-10-20 21:23:53 +0200

    basamamak ornegi

---
+++ 6 basamakli sayinin sayi degerlerini toplama/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_basamakli_sayinin_sayi_degerlerini_toplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 523495;
+
+            int birler = sayi % 10;
+            int onlar = (sayi % 100)/10;
+            int yuzler = (sayi % 1000)/100;
+            int binler = (sayi % 10000) / 1000;
+            int onbinler = (sayi % 100000) /10000;
+            int yuzbinler = sayi / 100000;
+
+            int sayiDegerleri = (birler + onlar+yuzler+binler + onbinler + yuzbinler);
+
+            Console.WriteLine(sayiDegerleri);
+        }
+    }
+}
