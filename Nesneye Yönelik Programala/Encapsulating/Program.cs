e7f754a08adce7bf3888767808b6ad9c945f31e4
blaze187 <frhtdvm@gmail.com>
2021-11-20 11:50:50 +0100

    tüm dosyalar güncellendi

---
+++ Nesneye Yönelik Programala/Encapsulating/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Encapsulating
+{
+    class Program
+    {
+        class Futbolcu
+        {
+            private int bonservisBedeli;
+            public int getBonservisBedeli(){
+                return bonservisBedeli;
+            }
+            public void setBonservisBedeli(int fiyat){
+                this.bonservisBedeli = fiyat;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Futbolcu f1 = new Futbolcu();
+            f1.setBonservisBedeli(150000000);
+
+            Console.WriteLine(f1.getBonservisBedeli());
+        }
+    }
+}
---
+++ Nesneye Yönelik Programala/kapsüllemeOrnek2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace kapsüllemeOrnek2
+{
+    class Program
+    {
+        class BankHesap{
+            int bakiye;
+
+            public void paraYatir(int miktar){
+                bakiye += miktar;
+            }
+            public void paraCekme(int miktar){
+                bakiye -= miktar;
+            }
+            public int bakiyeSorgula()
+            {
+                return bakiye;
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankHesap b1 = new BankHesap();
+            Console.WriteLine(b1.bakiyeSorgula());
+
+            b1.paraYatir(1500);
+            Console.WriteLine(b1.bakiyeSorgula());
+            b1.paraCekme(750);
+            Console.WriteLine(b1.bakiyeSorgula());
+
+        }
+    }
+}
