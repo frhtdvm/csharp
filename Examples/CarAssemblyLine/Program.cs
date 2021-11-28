cc131b79ed714616a85d58dd974fac41e2e4a325
blaze187 <frhtdvm@gmail.com>
2021-11-28 13:19:40 +0100

    exercism araba ornegi

---
+++ Examples/CarAssemblyLine/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CarAssemblyLine
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+}
+
+    class Program : NewBaseType
+    {
+        public static double carsPerHour = 221;
+        public static double SuccessRate(int speed)
+        {
+
+        if(1 <= speed && speed <=4) {return 1.0;}
+        if(5 <= speed && speed <=8) {return 0.9;}
+        if(9 == speed) {return 0.8;}
+        if(10 == speed) {return 0.77;}
+        return speed;
+        }
+        
+        public static double ProductionRatePerHour(int speed)
+        {
+            return speed * carsPerHour *SuccessRate(speed); 
+        }
+
+        public static int WorkingItemsPerMinute(int speed)
+        {
+            return (int)ProductionRatePerHour(speed) /60;
+        }
+      }
+    }
+}
+    
