14e982f5d4aa71e46beecfe5cc22ba7f84be0c7a
blaze187 <frhtdvm@gmail.com>
2021-11-28 11:02:56 +0100

    after dotnet 5 reupdate

---
+++ Examples/CarAssambly/Program.cs
@@ -0,0 +1,24 @@
+﻿      int speed = 2;
+    int cars = 221;
+    int uretim = 0;
+
+     if(speed >= 1 && speed<=4)
+    {
+        //speed * cars = uretim;
+        Console.WriteLine("Üretim = " + speed);
+
+// first thew variables 
+/* double rate = 0;
+int hour = 0;
+int minute = 0; */
+
+
+/* product perMinute()
+{
+
+} */
+/* public int productonPerHour(int speed)
+{
+    
+} 
+ */
